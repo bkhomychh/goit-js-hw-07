5cb5eccd985c9653334bca3aea53dd38dcb3c06d
Bohdan Khomych <bod.khomych@gmail.com>
2022-12-30 16:59:53 +0200

    Corrects a template string

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -18,9 +18,8 @@ function makeGalleryMarkup(arr) {
         item => `<a class='gallery__item' href='${item.original}'>
 			<img
 				class='gallery__image'
-				src=${item.preview} 
-				alt='${item.description}'>
-		</a>`,
+				src='${item.preview}'
+				alt='${item.description}'></a>`,
       )
       .join(''),
   );
